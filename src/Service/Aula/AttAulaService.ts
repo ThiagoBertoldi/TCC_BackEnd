5952d785f9fdfda56ad6a2744a54cfcfce10ce4a
Thiago Bertoldi <thiago.bertoldi@catolicasc.edu.br>
2023-11-29 20:27:06 -0300

    Ajuste SetBackgroundImage

--- src/Service/Aula/AttAulaService.ts
+++ src/Service/Aula/AttAulaService.ts
@@ -17,7 +17,7 @@ class SaveAulasService {
    async execute({ elementsSave, backgroundBase64, id_materia }: SaveAulas) {
       if (!elementsSave || elementsSave.length === 0)
          throw new Error('Nada gravado')
-      
+
       elementsSave.map(async item => {
          let filter = { _id: new ObjectId(item.id_aula) }
          let att = { $set: { posicaoX: item.posicaoX, posicaoY: item.posicaoY } }
@@ -27,13 +27,15 @@ class SaveAulasService {
 
       let idMateria = new ObjectId(id_materia);
 
-      let existeImagem = await client.collection('BackgroundMateria').findOne({ idMateria })
-      if(existeImagem?._id) {
-         let filter = { _id: existeImagem._id }
-         let att = { $set: { backgroundBase64 } }
-         await client.collection('BackgroundMateria').updateOne(filter, att)
-      } else {
-         await client.collection('BackgroundMateria').insertOne({ backgroundBase64, idMateria  })
+      if (backgroundBase64 != "http://localhost:8080/aula-professor/null") {
+         let existeImagem = await client.collection('BackgroundMateria').findOne({ idMateria })
+         if (existeImagem?._id) {
+            let filter = { _id: existeImagem._id }
+            let att = { $set: { backgroundBase64 } }
+            await client.collection('BackgroundMateria').updateOne(filter, att)
+         } else {
+            await client.collection('BackgroundMateria').insertOne({ backgroundBase64, idMateria })
+         }
       }
 
       return null;
